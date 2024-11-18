612ba095ae87c6d2e5b667f0cec3b108658dfba4
FarhanaBadubhai <bf99584@gmail.com>
2024-11-18 13:05:51 +0530

    color added for font

--- lib/consts/global_colors.dart
+++ lib/consts/global_colors.dart
@@ -6,4 +6,5 @@ Color lightCardColor = const Color(0xFFFfFFFf); //F0FFFF
 Color lightBackgroundColor = const Color(0xFFD0E8F2);
 Color lightIconsColor = const Color.fromARGB(255, 180, 221, 67);
 Color lightTextColor = const Color(0xff324558);
+Color lightFontTextColor = const Color.fromARGB(255, 87, 11, 163);
 
